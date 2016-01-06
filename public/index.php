f44e26789dc235afaf645e287ac6cc54e73c6fcb
Emanuel Canha <ecanha@trojanutilities.co.uk>
2016-01-06 13:46:32 +0000

    changed form

--- public/index.php
+++ public/index.php
@@ -3,24 +3,19 @@
 <HTML>
     <HEAD>
         <TITLE>Twilio</TITLE>
-        
+
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" 
               integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-        
+
     </HEAD>
     <BODY>
-
         <?php
-// Install the library via PEAR or download the .zip file to your project folder.
-// This line loads the library
         require('Services/Twilio.php');
 
         $sid = "AC1bd01d0304ada92d34175d05fb42afe8"; // Your Account SID from www.twilio.com/user/account
         $token = "27f226f3a4457865b8493d8b82812865"; // Your Auth Token from www.twilio.com/user/account
-        ?>
 
-        <?php if (!empty($_POST)): ?>
-        <?php
+        if (!empty($_POST)) {
             $client = new Services_Twilio($sid, $token);
             try {
                 $message = $client->account->messages->create(array(
@@ -29,30 +24,23 @@
                     'Body' => $_POST['text']
                 ));
                 
-                print $message->sid;
-
                 echo 'Message Sent!';
                 
             } catch (Services_Twilio_RestException $e) {
                 echo $e->getMessage();
             }
+        }
         ?>
-        
+
         <DIV>
-        
+
             <form action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> method="post">
                 Number: <input type="number" name="number"><br>
                 Text: <input type="text" name="text"><br>
                 <input type="button" class="btn btn-primary" value="Send">
             </form>
-            
-        </DIV>
 
-        <?php else: ?>
-        
-        
-        
-        <?php endif; ?>
+        </DIV>
 
     </BODY>
 </HTML>
