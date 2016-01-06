d1639ff32e8e13c8da07a3064e691f8f320990f9
Emanuel Canha <ecanha@trojanutilities.co.uk>
2016-01-06 11:41:20 +0000

    read body

--- public/reply.php
+++ public/reply.php
@@ -1,7 +1,15 @@
 <?php
     header("content-type: text/xml");
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+
+    if (POST['Body'] == 'YES') {
+    	echo "<Response>
+    	<Message>Hello, Mobile Monkey YES</Message>
+		</Response>";
+    
+    } else {
+    	echo "<Response>
+    	<Message>Hello, Mobile Monkey NO</Message>
+		</Response>";
+    }
 ?>
-<Response>
-    <Message>Hello, Mobile Monkey</Message>
-</Response>
