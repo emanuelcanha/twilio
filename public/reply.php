803d6d4e1448d6838070a60ce218888319cb6afc
Emanuel Canha <ecanha@trojanutilities.co.uk>
2016-01-06 11:53:40 +0000

    body read fix

--- public/reply.php
+++ public/reply.php
@@ -1,16 +1,16 @@
 <?php
-    header("content-type: text/xml");
-    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-
+    
   	<?php if (POST['Body'] == 'YES') {
     	$msg = 'yes'
     
     } else {
     	$msg = 'no'
     }
-    
+
+    header("content-type: text/xml");
+    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 
 <Response>
-    <Message><?php echo $msg . ' thanks for your reply' ?></Message>
+    <Message><?php echo $msg ?>, thanks for the message!</Message>
 </Response>
